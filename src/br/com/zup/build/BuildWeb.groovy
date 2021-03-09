d17759577a04034ebfda06515099c2b21be29cfb
Vinicius Sousa <csousa.vinicius@gmail.com>
2021-03-09 17:50:12 -0300

    [Flutter] new: build web step

---
+++ src/br/com/zup/build/BuildWeb.groovy
@@ -0,0 +1,36 @@
+
+package br.com.zup.build
+
+class BuildWeb{
+    def call (jenkins) {
+        jenkins.podTemplate(
+            containers: [
+                jenkins.containerTemplate(
+                    name: 'flutter', 
+                    image: 'flutter', 
+                    ttyEnabled: true, 
+                    command: 'cat'
+                )
+            ],
+            yamlMergeStrategy: jenkins.merge(),
+            workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
+                claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
+                readOnly: false
+            )
+        )
+
+        {
+            jenkins.node(jenkins.POD_LABEL){
+                jenkins.container('flutter'){
+                    try {
+                        jenkins.sh label: "Build Web", 
+                                   script: "flutter build web"
+                    } catch (Exception e) {
+                        jenkins.unstable("AN error occured during build step. Please, verify the logs.")
+                    }
+                    
+                }
+            }
+        }
+    }
+}
